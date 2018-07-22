d5e0119973d52b44bfa102701953987278245a76
vasamsettisaikiran <vasamsettisaikiran@yahoo.com>
2018-07-22 23:14:03 +0530

    modified naming and combined Async and Sync code in single class

--- AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
+++ AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
@@ -3,28 +3,27 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using AggregateGDPPopulation;
 using System.IO;
-using AggregateGDPPopulationAsync;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace AggregateGDPPopulation.Tests
 {
-    public class UnitTest1
+    public class AggregateGDPPopulationTesting
     {
         [Fact]
-        public void Test1()
+        public void SyncTesting()
         {
-            Class1.Method("../../../../AggregateGDPPopulation/data/datafile.csv");
+            Sync.CaculateGDPPopulation("../../../../AggregateGDPPopulation/data/datafile.csv");
             JObject Actual = JObject.Parse(File.ReadAllText("../../../../AggregateGDPPopulation/data/outputfilenew.json"));
             JObject Expected = JObject.Parse(File.ReadAllText("../../../expected-output.json"));
             Assert.Equal(Expected, Actual);           
         }
         [Fact]
-        public async void Test2()
+        public async void AsyncTesting()
         {
-            Task process1 =  Class2.Process("../../../../AggregateGDPPopulation/data/datafile.csv");
+            Task process1 =  Async.CalculateGDPPopulation("../../../../AggregateGDPPopulation/data/datafile.csv");
             await process1;
-            JObject Actual = JObject.Parse(File.ReadAllText("../../../../AggregateGDPPopulation/data/outputfileSync.json"));
+            JObject Actual = JObject.Parse(File.ReadAllText("../../../../AggregateGDPPopulation/data/outputfileAsync.json"));
             JObject Expected = JObject.Parse(File.ReadAllText("../../../expected-output.json"));
             Assert.Equal(Expected, Actual);
             Assert.Equal(Expected, Actual);
--- AggregateGDPPopulation/AggregateGDPPopulation.cs
+++ AggregateGDPPopulation/AggregateGDPPopulation.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
+
 namespace AggregateGDPPopulation
 {
-    public class Class1
+    public class Sync
     {
-        public static void Method(string FilePath)
+        public static void CaculateGDPPopulation(string FilePath)
         {
             Regex reg = new Regex("[\"]+");
             string line;
             StreamReader sample = new StreamReader(FilePath);
             JObject CountryVsContinent = JObject.Parse(File.ReadAllText("../../../../AggregateGDPPopulation/data/country-continent.json"));
-            List<string> rows = new List<string>();          
+            List<string> rows = new List<string>();
             while (!sample.EndOfStream)
             {
                 line = reg.Replace(sample.ReadLine(), "");
@@ -53,10 +55,52 @@ namespace AggregateGDPPopulation
                 }
                 catch { }
             }
-            string Json = JsonConvert.SerializeObject(resultset, Formatting.Indented);    
-            StreamWriter outputdata = new StreamWriter("../../../../AggregateGDPPopulation/data/outputfilenew.json");
-            outputdata.WriteLine(Json);
-            outputdata.Close();
+            string Json = JsonConvert.SerializeObject(resultset, Formatting.Indented);
+            File.WriteAllText("../../../../AggregateGDPPopulation/data/outputfilenew.json", Json);
+        }
+    }
+    public class Async
+    {
+        public static async Task CalculateGDPPopulation(string CSVFilePath)
+        {
+            Regex reg = new Regex("[\"]+");
+            List<string> rows = new List<string>(await File.ReadAllLinesAsync(CSVFilePath));
+            JObject CountryVsContinent = JObject.Parse(await File.ReadAllTextAsync("../../../../AggregateGDPPopulation/data/country-continent.json"));
+            string[] header;
+            header = reg.Replace(rows[0], "").Split(',');
+            rows.RemoveAt(0);
+            int CountryIndex = Array.IndexOf(header, "Country Name");
+            int PopulationIndex = Array.IndexOf(header, "Population (Millions) - 2012");
+            int GDPIndex = Array.IndexOf(header, "GDP Billions (US Dollar) - 2012");
+            Dictionary<string, Dictionary<string, double>> resultset = new Dictionary<string, Dictionary<string, double>>();
+            string[] rowdata;
+            foreach (string row in rows)
+            {
+                rowdata = reg.Replace(row, "").Split(',');
+                try
+                {
+                    string continent = CountryVsContinent[rowdata[CountryIndex]].ToString();
+
+                    if (resultset.ContainsKey(continent))
+                    {
+                        double GDPToAdd = Convert.ToDouble(rowdata[GDPIndex]);
+                        double PopulationToAdd = Convert.ToDouble(rowdata[PopulationIndex]);
+                        resultset[continent]["GDP_2012"] = resultset[continent]["GDP_2012"] + GDPToAdd;
+                        resultset[continent]["POPULATION_2012"] = resultset[continent]["POPULATION_2012"] + PopulationToAdd;
+                    }
+                    else
+                    {
+                        Dictionary<string, double> GDPVsPopulation = new Dictionary<string, double>();
+                        GDPVsPopulation.Add("GDP_2012", Convert.ToDouble(rowdata[GDPIndex]));
+                        GDPVsPopulation.Add("POPULATION_2012", Convert.ToDouble(rowdata[PopulationIndex]));
+                        resultset.Add(continent, GDPVsPopulation);
+                    }
+                }
+                catch { }
+            }
+            string Json = JsonConvert.SerializeObject(resultset, Formatting.Indented);
+            await File.WriteAllTextAsync("../../../../AggregateGDPPopulation/data/outputfileAsync.json", Json);
+
         }
     }
 }
